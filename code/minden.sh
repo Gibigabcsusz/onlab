8f713d57cdb7c190f62311606488c331902acc93
Gibigabcsusz <gbr.szilagyi@gmail.com>
2021-05-11 18:45:13 +0200

    ng mérés

--- code/minden.sh
+++ code/minden.sh
@@ -2,18 +2,17 @@
 
 g++ -std=c++11 -o bin/PM code/PM.cpp
 
-touch output/meres_ng
-> output/meres_ng
+touch output/meres_ng.dat
+> output/meres_ng.dat
 
 for number in 10 20 50 100 200 500 1000 2000 5000
 do
-bin/PM $number 300 >> output/meres_ng
+bin/PM $number 300 >> output/meres_ng.dat
 done
+
+
+
+
+gnuplot -c 'code/ngplotter'
 exit 0
 
-
-
-
-#gnuplot -c 'code/plotter'
-
-gnuplot set terminal eps; set output 'output/meres_ng.eps'; plot 'output/meres_ng' with lines gnuplot set terminal eps;
